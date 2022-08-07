4890a8dcbbd21d1745e23062d0f0d44c71ea76a8
hung <hungtrangia2003@gmail.com>
2022-08-07 18:06:35 +1000

    create initial user database

---
+++ user/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.models import User
+from user.models import UserProfileInfo
+
+
+class FormUser(forms.ModelForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={
+        "class": "form-control", 
+        "placeholder": "Username",
+    }))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={
+        "class": "form-control", 
+        "placeholder": "Email",
+    }))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={
+        "class": "form-control", 
+        "placeholder": "Password",
+    }))
+    confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={
+        "class": "form-control", 
+        "placeholder": "Confirm Password",
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class FormUserProfileInfo(forms.ModelForm):
+    portfolio = forms.URLField(label='Portfolio', required=False, widget=forms.TextInput(attrs={
+        "class": "form-control", 
+        "placeholder": "Portfolio",
+    }))
+    image = forms.ImageField(label='Image', required=False, widget=forms.FileInput(attrs={
+        "class": "form-control-file"
+    }))
+
+    class Meta:
+        model = UserProfileInfo
+        # fields = ('portfolio', 'image')
+        exclude = ('user',)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, reverse
+from django.core.paginator import Paginator
+from user.models import *
+from user.forms import FormUser, FormUserProfileInfo
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, JsonResponse
+from urllib.parse import urlencode
+
+from rest_framework import viewsets, permissions
+
+
 
 # Create your views here.
+
+def login(request):
+    if  request.user.is_authenticated:
+        return redirect('home:index')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)   # user
+            return redirect('home:index')
+    return redirect('home:index')
+
+def create_account(request):
+    pass
+        
+        
+def logout(request):
+    if  request.user.is_authenticated:
+        return redirect('home:index')
+    # form_user = FormUser()
+    # form_profile = FormUserProfileInfo()
+    logout(request)
+    return redirect('home:index')
+    
